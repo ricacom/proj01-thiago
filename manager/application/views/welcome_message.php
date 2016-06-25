be735be85155ff8befc28e7ad7337bebcbdad2b5
ricacom <ricacom@gmail.com>
2016-06-25 04:54:32 -0300

    ajust style welcome

--- manager/application/views/welcome_message.php
+++ manager/application/views/welcome_message.php
@@ -62,7 +62,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		margin: 10px;
 		border: 1px solid #D0D0D0;
 		box-shadow: 0 0 8px #D0D0D0;
-		width:50%;
+		width:40%;
 		margin: 30px auto;
 	}
 	</style>
